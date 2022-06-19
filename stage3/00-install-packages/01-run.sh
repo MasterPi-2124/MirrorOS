00b7b36015e08c6718d528579afa498b8de09c0b
Master Pi <minh.vln140501@gmail.com>
2022-06-19 17:14:00 +0700

    wt fgoing on???

--- stage3/00-install-packages/01-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-on_chroot << EOF
-    systemctl set-default graphical.target
-EOF
--- stage3/02-code-environment/00-run.sh
+++ stage3/02-code-environment/00-run.sh
@@ -1,26 +1,26 @@
-#!/bin/bash -e
+#!/bin/bash
 
 # go to home folder
 
 on_chroot << EOF
+	systemctl set-default graphical.target
 	python3 -m pip install --upgrade pip
 	python3 -m pip install keras numpy opencv-python regex imutils datetime
 	curl -sl https://deb.nodesource.com/setup_16.x | sudo -E bash
 	apt install -y nodejs 
 	npm install --location=global pm2
-EOF
+	git clone https://github.com/MichMich/MagicMirror 
 
-cd "${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
-git clone https://github.com/MichMich/MagicMirror 
-cd MagicMirror
-npm install --only=prod --omit=dev
-cp config/config.js.sample  config/config.js
-cd modules
-git clone https://github.com/bugsounet/EXT-Detector
-git clone https://github.com/bugsounet/MMM-GoogleAssistant
-git clone https://github.com/bugsounet/Gateway
-git clone https://github.com/bugsounet/EXT-Alert
-cd EXT-Detector && npm install
-cd ../Gateway && npm install
-cd ../EXT-Alert && npm install
-cd ../MMM-GoogleAssistant && npm install
+	cd MagicMirror
+	npm install --only=prod --omit=dev
+	wget -O config/config.js https://raw.githubusercontent.com/MasterPi-2124/SmartMirror/master/config/config.js
+	cd modules
+	git clone https://github.com/bugsounet/EXT-Detector
+	git clone https://github.com/bugsounet/MMM-GoogleAssistant
+	git clone https://github.com/bugsounet/Gateway
+	git clone https://github.com/bugsounet/EXT-Alert
+	cd EXT-Detector && npm install
+	cd ../Gateway && npm install
+	cd ../EXT-Alert && npm install
+	cd ../MMM-GoogleAssistant && npm install
+EOF
