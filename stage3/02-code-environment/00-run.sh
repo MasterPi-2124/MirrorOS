65989ff6fe50e0cd9869a28582f3616a50e4bee9
Master Pi <minh.vln140501@gmail.com>
2022-06-19 15:56:57 +0700

    dunno

--- stage3/02-code-environment/00-run.sh
+++ stage3/02-code-environment/00-run.sh
@@ -7,20 +7,20 @@ on_chroot << EOF
 	python3 -m pip install keras numpy opencv-python regex imutils datetime
 	curl -sl https://deb.nodesource.com/setup_16.x | sudo -E bash
 	apt install -y nodejs 
-	npm install -g pm2
-	echo $(ls -a)
-	cd ~
-	git clone https://github.com/MichMich/MagicMirror 
-	cd MagicMirror
-	npm install --only=prod --omit=dev
-	cp config/config.js.sample  config/config.js
-	cd modules
-	git clone https://github.com/bugsounet/EXT-Detector
-	git clone https://github.com/bugsounet/MMM-GoogleAssistant
-	git clone https://github.com/bugsounet/Gateway
-	git clone https://github.com/bugsounet/EXT-Alert
-	cd EXT-Detector && npm install
-	cd ../Gateway && npm install
-	cd ../EXT-Alert && npm install
-	cd ../MMM-GoogleAssistant && npm install
+	npm install --location=global pm2
 EOF
+
+cd "${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
+git clone https://github.com/MichMich/MagicMirror 
+cd MagicMirror
+npm install --only=prod --omit=dev
+cp config/config.js.sample  config/config.js
+cd modules
+git clone https://github.com/bugsounet/EXT-Detector
+git clone https://github.com/bugsounet/MMM-GoogleAssistant
+git clone https://github.com/bugsounet/Gateway
+git clone https://github.com/bugsounet/EXT-Alert
+cd EXT-Detector && npm install
+cd ../Gateway && npm install
+cd ../EXT-Alert && npm install
+cd ../MMM-GoogleAssistant && npm install
