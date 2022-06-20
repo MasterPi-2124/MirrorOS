5079d0a98cd18e18cb521998c6df73852c4d1329
Master Pi <minh.vln140501@gmail.com>
2022-06-20 14:32:14 +0700

    random code

--- stage3/02-code-environment/00-run.sh
+++ stage3/02-code-environment/00-run.sh
@@ -7,18 +7,21 @@ on_chroot << EOF
 	curl -sl https://deb.nodesource.com/setup_16.x | sudo -E bash
 	apt install -y nodejs 
 	npm install --location=global pm2
-	git clone https://github.com/MichMich/MagicMirror 
-
-	cd MagicMirror
-	npm install --only=prod --omit=dev
-	wget -O config/config.js https://raw.githubusercontent.com/MasterPi-2124/SmartMirror/master/config/config.js
-	cd modules
-	git clone https://github.com/bugsounet/EXT-Detector
-	git clone https://github.com/bugsounet/MMM-GoogleAssistant
-	git clone https://github.com/bugsounet/Gateway
-	git clone https://github.com/bugsounet/EXT-Alert
-	cd EXT-Detector && npm install
-	cd ../Gateway && npm install
-	cd ../EXT-Alert && npm install
-	cd ../MMM-GoogleAssistant && npm install
+	
 EOF
+
+cd "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}"
+git clone https://github.com/MichMich/MagicMirror 
+cd MagicMirror
+npm install --only=prod --omit=dev
+wget -O config/config.js https://raw.githubusercontent.com/MasterPi-2124/SmartMirror/master/config/config.js
+cd modules
+git clone https://github.com/bugsounet/EXT-Detector
+git clone https://github.com/bugsounet/MMM-GoogleAssistant
+git clone https://github.com/bugsounet/Gateway
+git clone https://github.com/bugsounet/EXT-Alert
+
+cd EXT-Detector 
+cd ../Gateway && npm install
+cd ../EXT-Alert && npm install
+cd ../MMM-GoogleAssistant && npm install
