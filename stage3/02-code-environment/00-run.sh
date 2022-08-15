c16abe9bad8c65a20a8f7715c73c2cf20ded036f
Master Pi <minh.vln140501@gmail.com>
2022-08-15 17:44:50 +0700

    Update 00-run.sh

--- stage3/02-code-environment/00-run.sh
+++ stage3/02-code-environment/00-run.sh
@@ -2,13 +2,14 @@
 
 on_chroot << EOF
 	systemctl set-default graphical.target
+	raspi-config nonint do_camera 0
 	python3 -m pip install --upgrade pip
 	python3 -m pip install keras numpy opencv-python regex imutils datetime
 	curl -sl https://deb.nodesource.com/setup_16.x | sudo -E bash
 	apt install -y nodejs libhdf5-dev libc-ares-dev libeigen3-dev libopenblas-dev libblas-dev openmpi-bin libopenmpi-dev gfortran liblapack-dev sox libsox-fmt-all libmagic-dev
 	npm install --location=global pm2 uuid@latest
-	SUDO_USER="${FIRST_USER_NAME}" git config --global user.name "${GIT_USERNAME}"
-	SUDO_USER="${FIRST_USER_NAME}" git config --global user.email "${GIT_EMAIL}"
+	git config --global user.name "${GIT_USERNAME}"
+	git config --global user.email "${GIT_EMAIL}"
 	pip3 install gdown pybind11 seaborn
 	pip3 install keras_applications==1.0.8 --no-deps
 	pip3 install keras_preprocessing==1.1.1 --no-deps
